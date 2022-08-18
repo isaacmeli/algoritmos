e44848a69ac8d16a554165826037fe3fd20aa9de
Isaac Nunes <isaac.nunes@mercadolivre.com>
2022-08-18 16:26:24 -0300

    Adicionando algoritmo de Pilhas

--- stacks/stack.go
+++ stacks/stack.go
@@ -1,26 +1,25 @@
 package stacks
 
-type Stack struct {
-	queue []int
-	len   int
-}
+type Stack struct{ queue []int }
 
 func (stack *Stack) empty() bool {
-	if stack.len == 0 {
+	if stack.len() == 0 {
 		return true
 	} else {
 		return false
 	}
 }
 
+func (stack *Stack) len() int {
+	return len(stack.queue)
+}
+
 func (stack *Stack) Push(item int) {
 	stack.queue = append(stack.queue, item)
-	stack.len = len(stack.queue)
 }
 
 func (stack *Stack) Pop() {
 	if !stack.empty() {
-		stack.len = stack.len - 1
-		stack.queue = stack.queue[:stack.len]
+		stack.queue = stack.queue[:stack.len()-1]
 	}
 }
